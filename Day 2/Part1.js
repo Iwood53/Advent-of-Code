6fab6dd20a7a3ba931c9f5a23780750c9fbd5799
Iwood53 <Iwood1350@gmail.com>
2020-12-02 13:53:50 -0500

    day 2 part 1

---
+++ Day 2/Part1.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+var passwordsRaw = fs.readFileSync('raw.csv', 'utf-8').split(/\r\n/);
+
+console.time('main')
+
+counter = 0;
+
+passwordsRaw.forEach(element => {
+    var dirtyRow = element.split(/\s|:|-/);
+    var occurances = dirtyRow[4].split(dirtyRow[2]).length - 1;
+
+    if (occurances >= dirtyRow[0] && occurances <= dirtyRow[1]){
+        counter += 1;
+    }
+    
+})
+
+console.timeEnd('main');
+console.log(counter);
