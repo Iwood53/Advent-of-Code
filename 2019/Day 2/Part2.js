8cf1877bf1eeff93f390b97649dea233560c98ea
Iwood53 <Iwood1350@gmail.com>
2020-12-04 17:14:34 -0500

    Update Part2.js

--- 2019/Day 2/Part2.js
+++ 2019/Day 2/Part2.js
@@ -20,8 +20,7 @@ function recursiveCompute(input, startIndex = 0){
 }
 
 function findTargetVar(target, input, nounLower = 0, nounUpper = 99, verbLower = 0, verbUpper = 99){
-    for (var n = nounLower; n <= nounUpper; n++)
-    {
+    for (var n = nounLower; n <= nounUpper; n++){
         for (var v = verbLower; v <= verbUpper; v++){
             let currentInput = Object.assign({}, input)
             currentInput[1] = n;
