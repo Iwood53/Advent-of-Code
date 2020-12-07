a017435484a0f0096a81a2d56298c726b544bad1
Iwood53 <Iwood1350@gmail.com>
2020-12-07 15:27:12 -0500

    part2 Incomplete

---
+++ 2020/Day 7/Part2.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+var bags = fs.readFileSync('raw.txt', 'utf-8').split(/\r\n/);
+
+var possibleBags = getPossibleBagCount('shiny gold', parseBags(bags));
+
+var answer = 0;
+possibleBags.forEach(bag => {
+    //console.log(bag)
+})
+console.log(answer);
+
+function parseBags(bags, parsedBagList = []){
+    var targetBag = bags[0].split(" bags contain ")[0];
+    var subBags = bags[0].split(" bags contain ")[1].split(/[,.]/);
+    var subBagsJson = []
+    
+    subBags.forEach(subBag => {
+        var cleanBag = subBag.trim().replace('.', '');
+        cleanBag = cleanBag.substring(0, cleanBag.lastIndexOf(' '))
+        if (cleanBag[0]){
+            if (cleanBag[0] != 'n'){
+                subBagsJson.push({ bag : cleanBag.substring(2), count : cleanBag[0]})
+            }
+        }       
+    });
+
+    parsedBagList.push({target_bag : targetBag, subBags : subBagsJson});
+    
+    bags.shift();
+    if (bags.length == 0){ return parsedBagList }
+    return parseBags(bags, parsedBagList);
+} 
+
+function getPossibleBagCount(targetBag, fullBagList, matchingBagList = [])
+{    
+    fullBagList.forEach(record =>{      
+        if (record.subBags.some(e => e.bag === targetBag)){
+            if (!matchingBagList.includes(record)){ matchingBagList.push(record) }
+            return getPossibleBagCount(record.target_bag, fullBagList, matchingBagList);  
+        }            
+    })
+    return matchingBagList;
+}
+
