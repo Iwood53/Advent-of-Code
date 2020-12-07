b26a97333b2afbcdb1278d8b6ba930f489acd6ba
Iwood53 <Iwood1350@gmail.com>
2020-12-07 07:54:48 -0500

    Update Part1.js

--- 2019/Day 3/Part1.js
+++ 2019/Day 3/Part1.js
@@ -4,18 +4,18 @@ var rawWireDiagram = Object.values(fs.readFileSync('raw.txt', 'utf-8').split("\n
 rawWireDiagram.forEach(wire => { console.log(getWirePoints(wire.split(','))) });
 
 function lineSegmentIntersections(x11, y11, x12, y12, x21, y21, x22, y22){
-    //check parallel vs perpend
-    if ((x11 - x12 == 0 && x21 - x22 == 0) || (y11 - y12 == 0 && y21 - y22 == 0)){
-        //parallel
-    }
-    else {
-        //perpend
-    }
+    var parallelX = (x11 - x12 == 0 && x21 - x22 == 0) ? true : false;
+    var parallelY = (y11 - y12 == 0 && y21 - y22 == 0) ? true : false;
+    
+
+}
+
+function getParallelOverlapPoints(line1point1, line1point2, line2point1, line2point2, fixedPoint){
+
 }
 
 
-function getWirePoints(wires, currentPoint = [0,0], wirePoints = []){
-    
+function getWirePoints(wires, currentPoint = [0,0], wirePoints = []){   
     if (wires[0][0] == 'U'){ currentPoint[1] +=  +wires[0].substring(1) }
     if (wires[0][0] == 'D'){ currentPoint[1] -=  +wires[0].substring(1) }
     if (wires[0][0] == 'R'){ currentPoint[0] +=  +wires[0].substring(1) }
@@ -27,5 +27,3 @@ function getWirePoints(wires, currentPoint = [0,0], wirePoints = []){
     if (wires.length != 0){ return getWirePoints(wires, currentPoint, wirePoints) }
     return wirePoints;
 }
-
-
