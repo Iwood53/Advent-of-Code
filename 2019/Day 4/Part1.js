89ba2f799cbd7573de8d9784af5a4970314a196c
Iwood53 <Iwood1350@gmail.com>
2020-12-08 20:41:41 -0500

    Update Part1.js

--- 2019/Day 4/Part1.js
+++ 2019/Day 4/Part1.js
@@ -7,9 +7,7 @@ console.log(answer);
 
 function testPassword(password, lastDigit = 0){
     if (String(password).length == 6){   
-        if (/(00|11|22|33|44|55|66|77|88|99)/.test(String(password)) == false){            
-            return false;
-        }
+        if (/(\d)\1{1}/.test(String(password)) == false){ return false; }
     }
     if (String(password)[0] < lastDigit) { return false }
 
