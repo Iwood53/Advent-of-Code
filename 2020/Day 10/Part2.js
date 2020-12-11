af9062c9df0d59f148fb6ecfbed602eb85061c77
Iwood53 <Iwood1350@gmail.com>
2020-12-11 12:18:40 -0500

    Update Part2.js

--- 2020/Day 10/Part2.js
+++ 2020/Day 10/Part2.js
@@ -1,19 +1,21 @@
 const fs = require('fs');
 
-const input = fs.readFileSync('raw.txt', 'utf-8').split(/\r\n/).sort((a,b) => a - b);
 
+const input = fs.readFileSync('raw.txt', 'utf-8').split(/\r\n/).sort((a,b) => a - b);
 input.unshift(0)
+
 const lastAdaptor = +input[input.length - 1];
 
-var test = countPaths(input, lastAdaptor)
-console.log(test)
+var answer = countPaths(input, lastAdaptor)
+console.log(answer)
 
 function getSuitableAdaptorsDown(nodePower, allAdaptors){
     let suitableAdaptors = allAdaptors.filter(adaptor => (nodePower > +adaptor && +adaptor + 3 >= nodePower))
     return suitableAdaptors
 }
 
-function countPaths(adaptors, node, myMap = new Map(), pathCounter = 0){
+function countPaths(adaptors, node, myMap = new Map()){
+    let pathCounter = 0
     let suitableAdaptors = getSuitableAdaptorsDown(node, input);
     if (suitableAdaptors){
         suitableAdaptors.forEach(adaptor => {
