157c743fc2e5a44e83a0c1e5605a9861b12dd981
Iwood53 <Iwood1350@gmail.com>
2020-12-12 10:20:25 -0500

    Create Part1.js

---
+++ 2020/Day 12/Part1.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const input = fs.readFileSync('raw.txt', 'utf-8').split(/\r\n/)
+
+
+answer = getFinalDistanceFromOrgin(input);
+console.log(answer);
+
+
+function getFinalDistanceFromOrgin(input){
+    currentPoint = {x: 0, y: 0, heading: 90};    
+    input.forEach(currentInstruction => {
+        currentPoint = getNextPoint(currentInstruction, currentPoint);
+    });
+    return Math.abs(currentPoint.x) + Math.abs(currentPoint.y);
+}
+
+function getNextPoint(instruction, currentPoint){
+    let parsedInstruction = parseInstruction(instruction);
+    if (parsedInstruction.direction == 'N') { currentPoint.y += parsedInstruction.distance }
+    if (parsedInstruction.direction == 'S') { currentPoint.y -= parsedInstruction.distance}
+    if (parsedInstruction.direction == 'E') { currentPoint.x += parsedInstruction.distance}
+    if (parsedInstruction.direction == 'W') { currentPoint.x -= parsedInstruction.distance}
+    if (parsedInstruction.direction == 'L') { currentPoint.heading = rotateHeading(instruction, currentPoint) }
+    if (parsedInstruction.direction == 'R') { currentPoint.heading = rotateHeading(instruction, currentPoint) }
+    if (parsedInstruction.direction == 'F') {
+        if (currentPoint.heading == 0){ currentPoint.y += parsedInstruction.distance }
+        if (currentPoint.heading == 90){ currentPoint.x += parsedInstruction.distance }
+        if (currentPoint.heading == 180){ currentPoint.y -= parsedInstruction.distance }
+        if (currentPoint.heading == 270){ currentPoint.x -= parsedInstruction.distance }
+    }
+    return currentPoint;
+}
+
+function rotateHeading(instruction, currentPoint){
+    let parsedInstruction = parseInstruction(instruction)
+    if (parsedInstruction.direction == 'L'){ currentPoint.heading -= parsedInstruction.distance }
+    if (parsedInstruction.direction == 'R'){ currentPoint.heading += parsedInstruction.distance }
+    while(currentPoint.heading >= 360){ currentPoint.heading -= 360 }
+    while(currentPoint.heading < 0){ currentPoint.heading += 360 }
+    return currentPoint.heading;
+}
+
+function parseInstruction(instruction){
+    let direction = instruction[0];
+    let distance = instruction.substring(1)
+    return {direction: direction, distance: Number(distance)}
+}
