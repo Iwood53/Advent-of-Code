b9275370a935fc42bb84ac0837343b51c21ece0e
Ian Wood <Iwood1350@gmail.com>
2021-06-12 15:46:40 -0400

    part1 complete

--- 2019/Day 3/Part1.js
+++ 2019/Day 3/Part1.js
@@ -1,29 +1,44 @@
 const fs = require('fs');
 
-var rawWireDiagram = Object.values(fs.readFileSync('raw.txt', 'utf-8').split("\n"));
-rawWireDiagram.forEach(wire => { console.log(getWirePoints(wire.split(','))) });
 
-function lineSegmentIntersections(x11, y11, x12, y12, x21, y21, x22, y22){
-    var parallelX = (x11 - x12 == 0 && x21 - x22 == 0) ? true : false;
-    var parallelY = (y11 - y12 == 0 && y21 - y22 == 0) ? true : false;
-    
+var rawWireDiagram = Object.values(fs.readFileSync('raw.txt', 'utf-8').split("\r\n"));
 
-}
+var wire1Points = getPointsWireOccupies(rawWireDiagram[0].split(","));
+var wire2Points = getPointsWireOccupies(rawWireDiagram[1].split(","));
 
-function getParallelOverlapPoints(line1point1, line1point2, line2point1, line2point2, fixedPoint){
+var commonPoints = wire1Points.filter(point1 => {
+                        return wire2Points.some(point2 => {
+                            return ((point1.x == point2.x) && (point1.y == point2.y))
+                        })
+                    })
 
-}
+var distanceArray = [];
+commonPoints.forEach(point => {
+    distanceArray.push(Math.abs(point.x) + Math.abs(point.y))
+})
+
+var answer = distanceArray.reduce((a, b) => Math.min(a, b))
+console.log("Answer: " + answer);
 
 
-function getWirePoints(wires, currentPoint = [0,0], wirePoints = []){   
-    if (wires[0][0] == 'U'){ currentPoint[1] +=  +wires[0].substring(1) }
-    if (wires[0][0] == 'D'){ currentPoint[1] -=  +wires[0].substring(1) }
-    if (wires[0][0] == 'R'){ currentPoint[0] +=  +wires[0].substring(1) }
-    if (wires[0][0] == 'L'){ currentPoint[0] -=  +wires[0].substring(1) }
+function getPointsWireOccupies(instructions){
+    var currentPoint = {x: 0, y: 0};
+    var pointsArray = [];
 
-    wirePoints.push(currentPoint.map(x => x));
-    wires.shift();
+    instructions.forEach(command => {
+        var direction = command.substring(0, 1);
+        var magnitude = parseInt(command.substring(1));
 
-    if (wires.length != 0){ return getWirePoints(wires, currentPoint, wirePoints) }
-    return wirePoints;
+        for (i = magnitude - 1; i >= 0; i--){
+        if (direction.toUpperCase() == 'U'){ currentPoint = {x: currentPoint.x, y: currentPoint.y + 1}; } 
+        if (direction.toUpperCase() == 'D'){ currentPoint = {x: currentPoint.x, y: currentPoint.y - 1}; } 
+        if (direction.toUpperCase() == 'R'){ currentPoint = {x: currentPoint.x + 1, y: currentPoint.y}; }
+        if (direction.toUpperCase() == 'L'){ currentPoint = {x: currentPoint.x - 1, y: currentPoint.y}; }
+
+        pointsArray.push(currentPoint);
+
+        }
+    })
+
+    return pointsArray;
 }
